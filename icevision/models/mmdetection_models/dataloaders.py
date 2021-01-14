a417e47215af1e1fa973635df30bb3dec38cf539
lgvaz <lgvaz42@gmail.com>
2021-01-14 09:06:28 -0300

    refactors dataloaders

--- icevision/models/mmdetection_models/dataloaders.py
+++ icevision/models/mmdetection_models/dataloaders.py
@@ -50,12 +50,6 @@ def infer_dl(dataset, batch_tfms=None, **dataloader_kwargs) -> DataLoader:
     )
 
 
-def build_valid_batch(
-    records: Sequence[RecordType], batch_tfms=None
-) -> Tuple[dict, List[Dict[str, torch.Tensor]]]:
-    return build_train_batch(records=records, batch_tfms=batch_tfms)
-
-
 def build_train_batch(
     records: Sequence[RecordType], batch_tfms=None
 ) -> Tuple[dict, List[Dict[str, torch.Tensor]]]:
@@ -63,20 +57,10 @@ def build_train_batch(
 
     images, labels, bboxes, img_metas = [], [], [], []
     for record in records:
-        image, target = _build_train_sample(record)
-        images.append(image)
-        labels.append(target["labels"])
-        bboxes.append(target["boxes"])
-
-        img_c, img_h, img_w = image.shape
-        img_metas.append(
-            {
-                # height and width from sample is before padding
-                "img_shape": (record["height"], record["width"], img_c),
-                "pad_shape": (img_h, img_w, img_c),
-                "scale_factor": np.ones(4),  # TODO: is scale factor correct?
-            }
-        )
+        images.append(_img_tensor(record))
+        img_metas.append(_img_meta(record))
+        labels.append(_labels(record))
+        bboxes.append(_bboxes(record))
 
     data = {
         "img": torch.stack(images),
@@ -88,48 +72,52 @@ def build_train_batch(
     return data, records
 
 
-# TODO Refactor train_dl (this is from torchvision faster_rcnn)
-def _build_train_sample(
-    record: RecordType,
-) -> Tuple[torch.Tensor, Dict[str, torch.Tensor]]:
-    assert len(record["labels"]) == len(record["bboxes"])
-
-    image = im2tensor(record["img"])
-    target = {}
-
-    # TODO: Is negative sampling supported on mmdetection?
-    if len(record["labels"]) == 0:
-        target["labels"] = torch.zeros(0, dtype=torch.int64)
-        target["boxes"] = torch.zeros((0, 4), dtype=torch.float32)
-    else:
-        target["labels"] = tensor(record["labels"], dtype=torch.int64)
-        xyxys = [bbox.xyxy for bbox in record["bboxes"]]
-        target["boxes"] = tensor(xyxys, dtype=torch.float32)
-
-    return image, target
+def build_valid_batch(
+    records: Sequence[RecordType], batch_tfms=None
+) -> Tuple[dict, List[Dict[str, torch.Tensor]]]:
+    return build_train_batch(records=records, batch_tfms=batch_tfms)
 
 
 def build_infer_batch(records, batch_tfms=None):
     records = common_build_batch(records, batch_tfms=batch_tfms)
 
-    images, img_metas = [], []
+    imgs, img_metas = [], []
     for record in records:
-        image = im2tensor(record["img"])
-        images.append(image)
-
-        img_c, img_h, img_w = image.shape
-        img_metas.append(
-            {
-                # height and width from sample is before padding
-                "img_shape": (record["height"], record["width"], img_c),
-                "pad_shape": (img_h, img_w, img_c),
-                "scale_factor": np.ones(4),  # TODO: is scale factor correct?
-            }
-        )
+        imgs.append(_img_tensor(record))
+        img_metas.append(_img_meta(record))
 
     data = {
-        "img": [torch.stack(images)],
+        "img": [torch.stack(imgs)],
         "img_metas": [img_metas],
     }
 
     return data, records
+
+
+def _labels(record):
+    if len(record["labels"]) == 0:
+        raise RuntimeError("Negative samples still needs to be implemented")
+    else:
+        return tensor(record["labels"], dtype=torch.int64)
+
+
+def _bboxes(record):
+    if len(record["labels"]) == 0:
+        raise RuntimeError("Negative samples still needs to be implemented")
+    else:
+        xyxys = [bbox.xyxy for bbox in record["bboxes"]]
+        return tensor(xyxys, dtype=torch.float32)
+
+
+def _img_meta(record):
+    img_h, img_w, img_c = record["img"].shape
+    return {
+        # height and width from sample is before padding
+        "img_shape": (record["height"], record["width"], img_c),
+        "pad_shape": (img_h, img_w, img_c),
+        "scale_factor": np.ones(4),  # TODO: is scale factor correct?
+    }
+
+
+def _img_tensor(record):
+    return im2tensor(record["img"])
